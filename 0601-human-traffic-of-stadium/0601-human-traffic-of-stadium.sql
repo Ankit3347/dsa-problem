ba55d979486e8b7415d85ecfadca0d71cdc4a5ce
Ankit prajapati <149420362+Ankit3347@users.noreply.github.com>
2026-06-05 15:14:37 +0530

    Time: 284 ms (90.84%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,22 @@
+/* Write your T-SQL query statement below */
+WITH cte AS (
+    SELECT *,
+           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100
+),
+valid_groups AS (
+    SELECT grp
+    FROM cte
+    GROUP BY grp
+    HAVING COUNT(*) >= 3
+)
+SELECT id,
+       visit_date,
+       people
+FROM cte
+WHERE grp IN (
+    SELECT grp
+    FROM valid_groups
+)
+ORDER BY visit_date;
